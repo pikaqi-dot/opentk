36ce2681d96fb0f3a73a427b49eff74751a27f77
pikaqi-dot <hual83129@gmail.com>
2025-02-12 15:04:01 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace ShaderExample
+{
+    public class Program
+    {
+        private static Shader _shader;
+        private static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "OpenTK Shader Example"
+            };
+
+            using (var window = new GameWindow(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Load += OnLoad;
+                window.RenderFrame += (FrameEventArgs e) => OnRenderFrame(window, e);
+                window.Run();
+            }
+        }
+        private static void OnLoad()
+        {
+            // Use relative paths to the shaders
+            string vertexPath = Path.Combine("shaders", "vertex_shader.glsl");
+            string fragmentPath = Path.Combine("shaders", "fragment_shader.glsl");
+
+            _shader = new Shader(vertexPath, fragmentPath);
+        }
+        private static void OnRenderFrame(GameWindow window, FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // Render your scene here
+            _shader.Use();
+            Console.WriteLine("Rendering frame");
+
+            // Swap buffers
+            window.SwapBuffers();
+        }
+    }
+}
---
+++ Shader.cs
@@ -0,0 +1,63 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.IO;
+
+namespace ShaderExample
+{
+    public class Shader
+    {
+        public int Handle { get; private set; }
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            string vertexShaderSource = File.ReadAllText(vertexPath);
+            string fragmentShaderSource = File.ReadAllText(fragmentPath);
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, vertexShaderSource);
+            GL.CompileShader(vertexShader);
+            CheckShaderCompileStatus(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragmentShaderSource);
+            GL.CompileShader(fragmentShader);
+            CheckShaderCompileStatus(fragmentShader);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+            GL.LinkProgram(Handle);
+            CheckProgramLinkStatus(Handle);
+
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        private void CheckShaderCompileStatus(int shader)
+        {
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int status);
+            if (status == (int)All.False)
+            {
+                string infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Shader compilation failed: {infoLog}");
+            }
+        }
+
+        private void CheckProgramLinkStatus(int program)
+        {
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int status);
+            if (status == (int)All.False)
+            {
+                string infoLog = GL.GetProgramInfoLog(program);
+                throw new Exception($"Program linking failed: {infoLog}");
+            }
+        }
+    }
+}
